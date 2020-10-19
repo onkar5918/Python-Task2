27a2c5f9926f7c6ebb93284e00bd8af0a9689fb6
Onkar Kulkarni <72261780+onkar5918@users.noreply.github.com>
2020-10-19 22:58:38 +0530

    Add files via upload

---
+++ Task 10- While Loop.py
@@ -0,0 +1,5 @@
+# print 10 to 1 using while
+cnt=10
+while cnt>0:
+    print(cnt)
+    cnt -= 1
---
+++ Task 11-For Loop.py
@@ -0,0 +1,16 @@
+#Use of for loops
+# list1=[1,2,3,4,5,6,7,8,9,10]
+# for item in list1:
+#     print(item,end=' ')
+# print(end="\t\t")
+# set1= set(list1)
+# for item in set1:
+#     print(item, end=' ')
+# tup1=tuple(set1)
+# print(end="\t\t")
+# for item in tup1:
+#     print(item, end=' ')
+# dict1={'name':'Onkar','city':'Pune'}
+# print()
+# for cnt in dict1.values():
+#     print(cnt,end=" ")
